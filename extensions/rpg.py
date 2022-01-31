97b4dd177b4813d2fb1b2622ae1420241a5b525a
Vbalder7 <toritoab01@gmail.com>
2022-01-31 02:45:15 -0600

    added ability to roll Fate Core dice

--- extensions/rpg.py
+++ extensions/rpg.py
@@ -12,11 +12,30 @@ plugin = lightbulb.Plugin('RPG')
 @lightbulb.add_checks(lightbulb.guild_only)
 @lightbulb.option('sides', 'sides on the die')
 @lightbulb.command('r', 'rolls a set of dice', aliases=("roll", "dice"))
-@lightbulb.implements(lightbulb.PrefixCommand)
-async def cmd_roll(ctx) -> None:
+@lightbulb.implements(lightbulb.PrefixCommandGroup)
+async def cmd_roll(ctx: lightbulb.Context) -> None:
     sides = ctx.options.sides
     total = d20.roll(sides)
     await ctx.respond(f'{total}',reply=True, mentions_reply=True)
+    
+#Command for rolling Fate Core dice
+@cmd_roll.child
+@lightbulb.add_checks(lightbulb.guild_only)
+@lightbulb.option('op1','',default='')
+@lightbulb.command('fc','rolls Fate Core dice')
+@lightbulb.implements(lightbulb.PrefixSubCommand)
+async def cmd_FC(ctx: lightbulb.Context) -> None:
+    
+    dice = ['+1','-1','0','+1','-1','0']
+    op1 = ctx.options.op1
+    x = [random.randint(1,5) for i in range(4)]
+    a,b,c,d = x[0], x[1], x[2], x[3]
+    if op1 != '':
+        totals = (f'{dice[a]}, {dice[b]}, {dice[c]}, {dice[d]}, {op1}')
+        await ctx.respond(totals)
+    else:
+        totals = (f'{dice[a]}, {dice[b]}, {dice[c]}, {dice[c]}')
+        await ctx.respond(totals)
 
 #Command to generate random Character Stats
 @plugin.command
